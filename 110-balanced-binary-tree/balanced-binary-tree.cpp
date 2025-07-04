68ebc0cff845ef1a92be40394dec2c8b207b76cb
pooja170422 <162096703+pooja170422@users.noreply.github.com>
2025-07-04 09:44:09 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (56.57%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -12,11 +12,12 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return  height(root)!=-1;
+        return (height(root)!=-1);
+        
     }
     int height(TreeNode* root){
         if(root==NULL){
-            return 0 ;
+            return 0;
         }
 
         int lh=height(root->left);
@@ -27,7 +28,10 @@ public:
         if(rh==-1){
             return -1;
         }
-        if(abs(lh-rh)>1) return -1;
+
+        if(abs(lh-rh)>1){
+            return -1;
+        }
         return 1+max(lh,rh);
     }
 };
