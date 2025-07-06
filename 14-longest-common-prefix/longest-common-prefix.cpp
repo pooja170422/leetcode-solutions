5765d73f51d4218b8b1878af0b96b19aa05b71eb
pooja170422 <162096703+pooja170422@users.noreply.github.com>
2025-07-06 21:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (33.20%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,15 +5,17 @@ public:
         string first=strs[0];
         string last=strs[strs.size()-1];
         string result="";
-        for(int i=0;i< first.size();i++){
+        for(int i=0;i<first.size();i++){
             if(first[i]==last[i]){
                 result+=first[i];
+
             }
             else{
                 break;
             }
         }
         return result;
+
         
     }
 };
