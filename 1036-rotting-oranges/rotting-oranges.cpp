f33c049998cd3b093c8a0b5a1d67f0b04ac4467e
pooja170422 <162096703+pooja170422@users.noreply.github.com>
2025-07-22 10:30:26 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (47.39%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<pair<int, int>, int>> q;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        int cntfresh = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({{i, j}, 0});
+                    vis[i][j]=1;
+                }
+                if (grid[i][j] == 1) {
+                    cntfresh++;
+                }
+            }
+        }
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        int tm = 0;
+        while (!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+            tm = max(tm, t);
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                if (nrow >= 0 and nrow < m and ncol >= 0 and ncol < n and
+                    grid[nrow][ncol] == 1 and vis[nrow][ncol] == 0) {
+                    q.push({{nrow, ncol}, t + 1});
+                    vis[nrow][ncol]= 1;
+                    cntfresh--;
+                }
+            }
+        }
+        if (cntfresh == 0) {
+            return tm;
+        } else {
+            return -1;
+        }
+    }
+};
