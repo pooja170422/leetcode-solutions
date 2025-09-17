c574ef55a2f7b5e313c29438b21fdbabc629ab09
pooja170422 <162096703+pooja170422@users.noreply.github.com>
2025-09-17 11:33:58 +0530

    Time: 24 ms (34.22%) | Memory: 92.6 MB (42.78%) - LeetSync

---
+++ 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int smallestAbsent(vector<int>& nums) {
+        int n = nums.size();
+        long long sum = 0;
+        unordered_set<int> st;
+
+        for (int x : nums) {
+            sum += x;
+            st.insert(x);
+        }
+
+        double avg = (double)sum / n;
+        int candidate = (int)floor(avg) + 1;   
+        candidate = max(candidate, 1);         
+
+        while (st.count(candidate)) {
+            candidate++;
+        }
+        return candidate;
+
+    }
+};
