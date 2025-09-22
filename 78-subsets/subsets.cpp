9c3f7eb564fb58e1e027794e9f566c6678d39fe6
pooja170422 <162096703+pooja170422@users.noreply.github.com>
2025-09-22 18:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (80.91%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-
-    void find(int idx,vector<vector<int>>&ans,vector<int>&temp,vector<int>&nums){
-        if(idx==nums.size()){
+    void find(int ind, vector<int>& nums, vector<int>& temp,
+                     vector<vector<int>>& ans) {
+        if (ind == nums.size()) {
             ans.push_back(temp);
             return;
         }
-        temp.push_back(nums[idx]);
-        find(idx+1,ans,temp,nums);
+        temp.push_back(nums[ind]);
+        find(ind + 1, nums, temp, ans);
         temp.pop_back();
-         find(idx+1,ans,temp,nums);
-
+        find(ind + 1, nums, temp, ans);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ans;
-        vector<int>temp;
-        find(0,ans,temp,nums);
+        vector<vector<int>> ans;
+        vector<int> temp;
+        find(0, nums, temp, ans);
         return ans;
-
-        
     }
 };
