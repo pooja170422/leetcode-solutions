8af33d08bcc89e08bb0c92d4a6e90c569c9c3463
pooja170422 <162096703+pooja170422@users.noreply.github.com>
2025-10-06 19:33:58 +0530

    Time: 4 ms (92.06%) | Memory: 32.3 MB (63.61%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void dfs(int row,int col,int drow[],int dcol[],vector<vector<int>>& grid,int m,int n){
-        grid[row][col]='#';
+    void dfs(vector<vector<int>>& grid,int m,int n,int row,int col){
+        grid[row][col]=-1;
+        int drow[4]={-1,0,1,0};
+        int dcol[4]={0,1,0,-1};
         for(int i=0;i<4;i++){
             int nrow=row+drow[i];
             int ncol=col+dcol[i];
             if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and grid[nrow][ncol]==1){
-                dfs(nrow,ncol,drow,dcol,grid,m,n);
+                dfs(grid,m,n,nrow,ncol);
             }
+
+
         }
-
     }
-
     int numEnclaves(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 || j==0 ||i==m-1 || j==n-1){
+                if(i==0 || i==m-1 || j==0 || j==n-1){
                     if(grid[i][j]==1){
-                        dfs(i,j,drow,dcol,grid,m,n);
+                        dfs(grid,m,n,i,j);
                     }
                 }
             }
