895b72b634aabcd78e60f996eaf6addf6987ef11
pooja170422 <162096703+pooja170422@users.noreply.github.com>
2025-10-06 15:28:58 +0530

    Time: 3 ms (13.66%) | Memory: 18 MB (89.98%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    void dfs(int row, int col, int drow[], int dcol[], int inicolor,
-             int newcolor, vector<vector<int>>& image,
-             vector<vector<int>>& ans) {
-        ans[row][col] = newcolor;
-
-        int m = image.size();
-        int n = image[0].size();
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-            if (nrow >= 0 and nrow < m and ncol >=0 and ncol < n and
-                    image[nrow][ncol] ==inicolor and ans[nrow][ncol] != newcolor) {
-                dfs(nrow, ncol, drow, dcol, inicolor, newcolor, image, ans);
+    void dfs(vector<vector<int>>& image,int drow[],int dcol[], int color,int inicol, int row, int col,int m,int n){
+        image[row][col]=color;
+        for(int i=0;i<4;i++){
+            int nrow=row+drow[i];
+            int ncol=col+dcol[i];
+            if(nrow>=0 and nrow<m and ncol>=0 and ncol<n and image[nrow][ncol]==inicol ){
+                dfs(image,drow,dcol,color,inicol,nrow,ncol,m,n);
             }
         }
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int color) {
-
-        int inicolor = image[sr][sc];
-        if (inicolor == color) {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)   {
+        int m=image.size();
+        int n=image[0].size();
+        if(image[sr][sc]==color){
             return image;
         }
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-        vector<vector<int>> ans = image;
-        dfs(sr, sc, drow, dcol, inicolor, color, image, ans);
-        return ans;
+      
+        int inicol=image[sr][sc];
+        int drow[4]={-1,0,1,0};
+        int dcol[4]={0,1,0,-1};
+        dfs(image,drow,dcol,color,inicol,sr,sc,m,n);
+        return image;
+
+
+        
     }
 };
